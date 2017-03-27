c536035d597a6d0d38c3574702b9cdd3013e8d09
Gabriel Rodriguez <g-rodz@hotmail.com>
2017-03-27 14:09:25 -0400

    Update SessionCounter.js

--- SessionCounter.js
+++ SessionCounter.js
@@ -2,36 +2,44 @@
 $(document).ready(function () {
 
     var warningTime = 0;
-    //Increment Interval
+    //Intervalo para incrementacion
     var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
 
-    //Verify session method
+    //metodo de verificar sesion
     function timerIncrement() {
         warningTime++;
 
-        if (warningTime > 13) { // 13 = 14 minutes in a 15 minute timeout expiration
+        if (warningTime > 14) { getActiveSession(); }
+
+        if (warningTime > 13) { // 13 = 14 minutes
+
             setTimeout(function () {
                 $('#timeout-warning').modal('show');
-            }); 
-            //set warning time to 0 when hidden to prevent modal again
+            });
+            //set warning time to  0 when hidden to prevent modal again
             $('#timeout-warning').on('hidden.bs.modal', function () {
                 warningTime = 0;
             })
 
             $('#isActiveSession').click(function () {
-                debugger;
-                var url = '/Home/isActiveSession/'; //controller NET CORE(Identity Framework)
-                $.get(url)
-                       .error(function () { location.reload(); }) //on expired session
-                       .success(function () {
-                           warningTime = 0;
-                           return true // session isActive
-                       })
-                $('#timeout-warning').modal('hide');
-                warningTime = 0;
-                return false;
+                getActiveSession();
             });
         }
+    }
+
+    function getActiveSession() {
+        var url = '/Home/isActiveSession/';
+        $.get(url)
+               .error(function () {//on expired session
+                   location.reload();
+               })
+               .success(function () {
+                   warningTime = 0;
+                   return true //session isActive
+               })
+        $('#timeout-warning').modal('hide');
+        warningTime = 0;
+        return false;
     }
 
 });
